1f5d3573ec8f95a178819063681dd117858ee84b
Kiberio <dhiroshi3@gmail.com>
2021-10-06 22:57:23 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração do vetor
+            hospedes[] vect = new hospedes[10];
+            //Solicitando o numero de hospedes a ser registrado
+            Console.WriteLine("Digite o numero de hospedes que irão alugar quartos: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                Console.WriteLine(" ");
+                //Informações do hospede
+                Console.WriteLine($"Hospede #{i}");
+                Console.WriteLine("Nome");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Email");
+                string email = Console.ReadLine();
+                Console.WriteLine("Quarto");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new hospedes(nome, email);
+            }
+            Console.WriteLine("");
+            //Imprimir quartos ocupados
+            Console.WriteLine("Quartos Ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ ConsoleApp9/hospedes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class hospedes
+    {
+        //Declaração das variaveis da classe
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        //Declaração do construtor
+        public hospedes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        //Formatação do to string
+        public override string ToString()
+        {
+            return Nome + " , " + Email;
+        }
+    }
+}
